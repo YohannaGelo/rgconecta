f924755415e3bbb99ca2e3dfd1b2f00d724c0d2b
Yohanna Gelo <yohannagelo@gmail.com>
2025-04-29 13:41:17 +0200

    Característica: Implementación del registro de usuarios y nuevos componentes
    - Se actualizó angular.json para incluir nuevos recursos y estilos.
    - Se añadió el enrutamiento para los componentes de registro, nuevos usuarios y opiniones.
    - Se creó el componente Registro para el registro de usuarios con gestión de formularios.
    - Se implementaron los componentes NuevaOferta y NuevoUsuario.
    - Se añadió el componente Opiniones para mostrar opiniones.
    - Se mejoró el componente Navbar para aplicar estilos de navegación dinámicos según la ruta actual.
    - Se actualizaron las hojas de estilo para personalizar la temática y el estilo de los formularios.
    - Se añadieron las imágenes necesarias para la marca y el perfil del usuario.

---
+++ client/ruiz-gijon-conecta/src/app/nueva-oferta/nueva-oferta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-oferta',
+  standalone: false,
+  templateUrl: './nueva-oferta.component.html',
+  styleUrl: './nueva-oferta.component.scss'
+})
+export class NuevaOfertaComponent {
+
+}
--- client/ruiz-gijon-conecta/src/app/shared/components/navbar/navbar.component.ts
+++ client/ruiz-gijon-conecta/src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -12,8 +14,14 @@ export class NavbarComponent {
 
   isAuthenticated$: Observable<boolean>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.isAuthenticated$ = this.authService.isAuthenticated;
+  }
+
+  // Para cambiar los estilos del switch del header
+  isInOfertasSection(): boolean {
+    const ruta = this.router.url;
+    return ['/home', '/ofertas', '/nueva-oferta'].includes(ruta);
   }
 
   logout() {
