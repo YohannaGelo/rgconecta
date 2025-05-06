fe7d652d552bc78b76607dbefd8c78c0c60629fa
Yohanna Gelo <yohannagelo@gmail.com>
2025-05-06 10:58:20 +0200

    Previo actualización angular

---
+++ client/ruiz-gijon-conecta/src/app/auth/registro-profes/registro-profes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-profes',
+  standalone: false,
+  templateUrl: './registro-profes.component.html',
+  styleUrl: './registro-profes.component.scss'
+})
+export class RegistroProfesComponent {
+
+}
