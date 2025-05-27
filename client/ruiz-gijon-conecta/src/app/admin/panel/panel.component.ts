9109a60fccba077668d2650b6dc50c40cac28c5d
Yohanna Gelo <yohannagelo@gmail.com>
2025-05-27 21:59:51 +0200

    Primera revisión de estilos, ajustados componentes responsive

--- client/ruiz-gijon-conecta/src/app/admin/panel/panel.component.ts
+++ client/ruiz-gijon-conecta/src/app/admin/panel/panel.component.ts
@@ -9,16 +9,73 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './panel.component.scss',
 })
 export class PanelComponent {
+  // adminLinks = [
+  //   { label: 'Titulaciones', route: '/admin/titulos' },
+  //   { label: 'Tecnologías', route: '/admin/tecnologias' },
+  //   { label: 'Empresas', route: '/admin/empresas' },
+  //   { label: 'Ofertas', route: '/admin/ofertas' },
+  //   { label: 'Usuarios', route: '/admin/usuarios' },
+  //   { label: 'Alumnos', route: '/admin/alumnos' },
+  //   { label: 'Profesores', route: '/admin/profesores' },
+  //   { label: 'Opiniones', route: '/admin/opiniones' },
+  //   { label: 'Experiencias', route: '/admin/experiencias' },
+  // ];
+  // En tu componente TS
   adminLinks = [
-    { label: 'Titulaciones', route: '/admin/titulos' },
-    { label: 'Tecnologías', route: '/admin/tecnologias' },
-    { label: 'Empresas', route: '/admin/empresas' },
-    { label: 'Ofertas', route: '/admin/ofertas' },
-    { label: 'Usuarios', route: '/admin/usuarios' },
-    { label: 'Alumnos', route: '/admin/alumnos' },
-    { label: 'Profesores', route: '/admin/profesores' },
-    { label: 'Opiniones', route: '/admin/opiniones' },
-    { label: 'Experiencias', route: '/admin/experiencias' },
+    {
+      label: 'Titulaciones',
+      shortLabel: 'Titul.',
+      icon: 'bi-award',
+      route: '/admin/titulos',
+    },
+    {
+      label: 'Tecnologías',
+      shortLabel: 'Tecno.',
+      icon: 'bi-code-square',
+      route: '/admin/tecnologias',
+    },
+    {
+      label: 'Empresas',
+      shortLabel: 'Empr.',
+      icon: 'bi-building',
+      route: '/admin/empresas',
+    },
+    {
+      label: 'Ofertas',
+      shortLabel: 'Ofertas',
+      icon: 'bi-briefcase',
+      route: '/admin/ofertas',
+    },
+    {
+      label: 'Usuarios',
+      shortLabel: 'Users',
+      icon: 'bi-people',
+      route: '/admin/usuarios',
+    },
+    {
+      label: 'Alumnos',
+      shortLabel: 'Alumn.',
+      icon: 'bi-mortarboard',
+      route: '/admin/alumnos',
+    },
+    {
+      label: 'Profesores',
+      shortLabel: 'Prof.',
+      icon: 'bi-person-video2',
+      route: '/admin/profesores',
+    },
+    {
+      label: 'Opiniones',
+      shortLabel: 'Op.',
+      icon: 'bi-chat-square-text',
+      route: '/admin/opiniones',
+    },
+    {
+      label: 'Experiencias',
+      shortLabel: 'Exp.',
+      icon: 'bi-journal-text',
+      route: '/admin/experiencias',
+    },
   ];
 
   @ViewChild('confirmModal') confirmModal!: TemplateRef<any>;
