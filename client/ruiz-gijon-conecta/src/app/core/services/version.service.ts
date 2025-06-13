981bbc33566a90d2d74931b98c224d43a997ec95
Yohanna Gelo <yohannagelo@gmail.com>
2025-06-13 09:47:45 +0200

    Nuevo modal para actualizar

--- client/ruiz-gijon-conecta/src/app/core/services/version.service.ts
+++ client/ruiz-gijon-conecta/src/app/core/services/version.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UpdateModalComponent } from '../../shared/components/update-modal/update-modal.component';
 
 @Injectable({ providedIn: 'root' })
 export class VersionService {
   private currentHash: string = '';
   private buildDate: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private modalService: NgbModal) {}
 
   checkVersion(): void {
     const url = `${environment.frontUrl}/assets/version.json?cb=${Date.now()}`;
@@ -18,14 +20,28 @@ export class VersionService {
       })
       .subscribe({
         next: ({ commitHash, buildDate }) => {
+          // if (this.currentHash && this.currentHash !== commitHash) {
+          //   const shouldReload = confirm(
+          //     '⚡ Nueva versión disponible. ¿Actualizar ahora?'
+          //   );
+          //   if (shouldReload) {
+          //     window.location.reload();
+          //   }
+          // }
+
           if (this.currentHash && this.currentHash !== commitHash) {
-            const shouldReload = confirm(
-              '⚡ Nueva versión disponible. ¿Actualizar ahora?'
-            );
-            if (shouldReload) {
-              window.location.reload();
-            }
+            const modalRef = this.modalService.open(UpdateModalComponent, {
+              centered: true,
+              backdrop: 'static',
+            });
+            modalRef.componentInstance.commitHash = commitHash;
+            // modalRef.componentInstance.commitLink = `https://github.com/YohannaGelo/rgconecta/commits/${commitHash}`; // 🔁 Sustituye con tu URL real
+            modalRef.componentInstance.buildDate = buildDate;
+
+            // this.currentHash = commitHash;
+            // this.buildDate = buildDate;
           }
+
           this.currentHash = commitHash;
           this.buildDate = buildDate;
         },
