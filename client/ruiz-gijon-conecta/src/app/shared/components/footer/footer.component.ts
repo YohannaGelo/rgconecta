9d58bcbfe4a1218320f22e3872eb1d3a29478fca
Yohanna Gelo <yohannagelo@gmail.com>
2025-06-13 10:01:48 +0200

    Última prueba antes de fusionar IX

--- client/ruiz-gijon-conecta/src/app/shared/components/footer/footer.component.ts
+++ client/ruiz-gijon-conecta/src/app/shared/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AboutModalComponent } from '../about-modal/about-modal.component';
 import { ABOUT_INFO } from '../../../../environments/enviroment.about';
+import { UpdateModalComponent } from '../update-modal/update-modal.component';
 
 @Component({
   selector: 'app-footer',
@@ -17,4 +18,14 @@ export class FooterComponent {
   openAboutModal() {
     this.modalService.open(AboutModalComponent, { centered: true });
   }
+
+  // openUpdateModalManualmente() {
+  //   const modalRef = this.modalService.open(UpdateModalComponent, {
+  //     centered: true,
+  //     backdrop: 'static',
+  //   });
+
+  //   modalRef.componentInstance.commitHash = 'prueba123';
+  //   modalRef.componentInstance.buildDate = new Date().toISOString();
+  // }
 }
